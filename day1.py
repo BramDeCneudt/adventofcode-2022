172076830bed53013c91b14cdcf064474d32744d
Bram De Cneudt <bramdecneudt@gmail.com>
2022-12-04 18:09:24 +0100

    solved first day

---
+++ day1.py
@@ -0,0 +1,14 @@
+
+
+with open('input/day1.txt') as file:
+
+    totalAmountList = []
+    total = 0
+
+    for calorie in file:
+        if calorie == "\n":
+            totalAmountList.append(total)
+            total = 0
+        else:
+            total += int(calorie)
+    print(max(totalAmountList))
---
+++ day1_1.py
@@ -0,0 +1,22 @@
+
+
+with open('input/day1.txt') as file:
+
+    totalAmountList = []
+    total = 0
+
+    for calorie in file:
+        if calorie == "\n":
+            totalAmountList.append(total)
+            total = 0
+        else:
+            total += int(calorie)
+    # it doesn't add the last on to the list
+    totalAmountList.append(total)    
+
+    totalTopThree = 0
+    for maxIndex in range(3):
+        maxAmount = max(totalAmountList)
+        totalTopThree += maxAmount
+        totalAmountList.remove(maxAmount)
+    print(totalTopThree)
